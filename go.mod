401ed3e4c65a3a8e7dae63949683794fc8de733a
mohammedazez <muhamadaziz047@gmail.com>
2023-08-16 21:02:31 +0700

    change to postgre

--- go.mod
+++ go.mod
@@ -3,14 +3,17 @@ module coffee-valley-be
 go 1.19
 
 require (
+	github.com/google/uuid v1.3.0
 	github.com/labstack/echo/v4 v4.11.1
-	gorm.io/driver/mysql v1.5.1
+	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.3
 )
 
 require (
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 type Bean struct {
 	gorm.Model
-	ID              int    `gorm:"type:int;primary_key"`
+	ID              string `gorm:"type:varchar(255);primary_key"`
 	BeanName        string `gorm:"type:varchar(100);null" json:"bean_name"`
 	DescriptionBean string `gorm:"type:varchar(255);null" json:"description_name"`
 	PricePerUnit    string `gorm:"type:varchar(255);null" json:"price_per_unit"`
@@ -20,14 +22,14 @@ type Bean struct {
 
 type DailyBean struct {
 	gorm.Model
-	ID        int    `gorm:"type:int;primary_key"`
-	BeanID    int    `gorm:"type:int;bean_id"`
-	SalePrice string `gorm:"type:varchar(255);null" json:"sale_price"`
+	ID        string `gorm:"type:varchar(255);primary_key"`
+	BeanID    string `gorm:"type:varchar(255);null" json:"bean_id"`
+	SalePrice int    `gorm:"type:int;null" json:"sale_price"`
 }
 
 type Distributor struct {
 	gorm.Model
-	ID              int    `gorm:"type:int;primary_key"`
+	ID              string `gorm:"type:varchar(255);primary_key"`
 	DistributorName string `gorm:"type:varchar(255);null" json:"distributor_name"`
 	City            string `gorm:"type:varchar(100);null" json:"city"`
 	State           string `gorm:"type:varchar(100);null" json:"state"`
@@ -38,7 +40,7 @@ type Distributor struct {
 
 type Document struct {
 	gorm.Model
-	ID           int    `gorm:"type:int;primary_key"`
+	ID           string `gorm:"type:varchar(255);primary_key"`
 	Title        string `gorm:"type:varchar(100);null" json:"title"`
 	DocumentFile string `gorm:"type:varchar(255);null" json:"document_file"`
 	Author       string `gorm:"type:varchar(50);null" json:"author"`
@@ -59,14 +61,18 @@ func getBean(c echo.Context) error {
 		DescriptionBean string `json:"description_bean"`
 		SalePrice       string `json:"sale_price"`
 	}
-	db.Table("beans").
+	result := db.Table("beans").
 		Select("beans.bean_name, beans.description_bean, daily_beans.sale_price").
 		Joins("INNER JOIN daily_beans ON beans.id = daily_beans.bean_id").
 		Where("daily_beans.sale_price >= ?", 0).
 		Scan(&beans)
 
+	if result.Error != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "error fetching beans")
+	}
+
 	if len(beans) == 0 {
-		return echo.NewHTTPError(http.StatusNotFound, "beans not found")
+		return c.JSON(http.StatusNoContent, nil) // NoContent when no beans are found
 	}
 
 	return c.JSON(http.StatusOK, beans)
@@ -77,6 +83,28 @@ func getCatalogs(c echo.Context) error {
 	var beans []Bean
 	db.Find(&beans)
 	return c.JSON(http.StatusOK, beans)
+}
+
+func createCatalog(c echo.Context) error {
+	db := c.Get("db").(*gorm.DB)
+	beans := new(Bean)
+	if err := c.Bind(beans); err != nil {
+		return err
+	}
+	beans.ID = uuid.New().String()
+	db.Create(&beans)
+	return c.JSON(http.StatusCreated, beans)
+}
+
+func createDailyBeans(c echo.Context) error {
+	db := c.Get("db").(*gorm.DB)
+	bean := new(DailyBean)
+	if err := c.Bind(bean); err != nil {
+		return err
+	}
+	bean.ID = uuid.New().String()
+	db.Create(&bean)
+	return c.JSON(http.StatusCreated, bean)
 }
 
 func getDistributors(c echo.Context) error {
@@ -92,6 +120,7 @@ func createDistributor(c echo.Context) error {
 	if err := c.Bind(distributor); err != nil {
 		return err
 	}
+	distributor.ID = uuid.New().String()
 	db.Create(&distributor)
 	return c.JSON(http.StatusCreated, distributor)
 }
@@ -100,14 +129,22 @@ func updateDistributor(c echo.Context) error {
 	db := c.Get("db").(*gorm.DB)
 	id := c.Param("id")
 	var distributor Distributor
-	db.First(&distributor, id)
-	if distributor.ID == 0 {
-		return echo.NewHTTPError(http.StatusNotFound, "distributor not found")
+
+	if err := db.First(&distributor, "id = ?", id).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return echo.NewHTTPError(http.StatusNotFound, "Distributor not found")
+		}
+		return err
 	}
+
 	if err := c.Bind(&distributor); err != nil {
 		return err
 	}
-	db.Save(&distributor)
+
+	if err := db.Save(&distributor).Error; err != nil {
+		return err
+	}
+
 	return c.JSON(http.StatusOK, distributor)
 }
 
@@ -117,6 +154,7 @@ func uploadDocument(c echo.Context) error {
 	if err := c.Bind(document); err != nil {
 		return err
 	}
+	document.ID = uuid.New().String()
 	db.Create(&document)
 	return c.JSON(http.StatusCreated, document)
 }
@@ -126,8 +164,8 @@ func deleteUser(c echo.Context) error {
 	id := c.Param("id")
 	var user Bean
 	db.First(&user, id)
-	if user.ID == 0 {
-		return echo.NewHTTPError(http.StatusNotFound, "User not found")
+	if user.ID == "" {
+		return echo.NewHTTPError(http.StatusNotFound, "user not found")
 	}
 	db.Delete(&user)
 	return c.NoContent(http.StatusNoContent)
@@ -140,9 +178,19 @@ func getUsers(c echo.Context) error {
 	return c.JSON(http.StatusOK, user)
 }
 
+func registerUsers(c echo.Context) error {
+	db := c.Get("db").(*gorm.DB)
+	user := new(Users)
+	if err := c.Bind(user); err != nil {
+		return err
+	}
+	db.Create(&user)
+	return c.JSON(http.StatusCreated, user)
+}
+
 func main() {
-	dsn := "root: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@tcp(127.0.0.1:3306)/simple-app?charset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := "host=floppy.db.elephantsql.com user=yffnrgkx password=EKOZpd-IXW3ZZN_nnG-wM2OGy7Sz4N4A dbname=yffnrgkx port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -162,12 +210,15 @@ func main() {
 
 	e.GET("/bean", getBean)
 	e.GET("/catalogs", getCatalogs)
+	e.POST("/catalog", createCatalog)
+	e.POST("/daily-beans", createDailyBeans)
 	e.GET("/distributors", getDistributors)
 	e.POST("/distributor", createDistributor)
 	e.PUT("/distributor/:id", updateDistributor)
 	e.POST("/document", uploadDocument)
 	e.DELETE("/users/:id", deleteUser)
 	e.GET("/users", getUsers)
+	e.POST("/users", registerUsers)
 
 	e.Start(":8080")
 }
